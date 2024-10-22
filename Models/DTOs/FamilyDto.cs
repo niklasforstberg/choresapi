c2c3317254cbeba5c220c11a05c54ec3b97668b2
niklas.forstberg <github@forstberg.com>
2024-10-22 22:48:56 +0200

    Made name not req

--- Models/DTOs/FamilyDto.cs
+++ Models/DTOs/FamilyDto.cs
@@ -3,7 +3,7 @@ namespace ChoresApi.Models.DTOs
     public class FamilyDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
     }
